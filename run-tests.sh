193293b465f7fa53f4720adc44fdcb332d467066
Nicola Tarocco <ntarocco@gmail.com>
2020-10-19 14:27:13 +0200

    global: upgrade to pytest-invenio 1.4

--- run-tests.sh
+++ run-tests.sh
@@ -2,16 +2,15 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2016-2019 CERN.
+# Copyright (C) 2016-2020 CERN.
 # Copyright (C) 2020 Northwestern University.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-
-pydocstyle invenio_previewer && \
-isort invenio_previewer tests --check-only --diff && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-pytest && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+python -m check_manifest --ignore ".travis-*" && \
+python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+python -m pytest
+python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
+tests_exit_code=$?
+exit "$tests_exit_code"
