d6a8bdab124dedf4fcdfd4ca36706b5180d5048f
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-11-29 01:36:05 +0100

    github: test and publish workflows

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".travis-*" && \
+python -m check_manifest --ignore ".*-requirements.txt" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
 python -m pytest
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
