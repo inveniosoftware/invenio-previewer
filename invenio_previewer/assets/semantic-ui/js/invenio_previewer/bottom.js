aad5338f0fd5981b47ffe26b1308f1a637a256a9
Pablo Panero <pablo.panero@cern.ch>
2020-12-09 13:27:53 +0100

    assets: fix import on js file

--- invenio_previewer/assets/semantic-ui/js/invenio_previewer/bottom.js
+++ invenio_previewer/assets/semantic-ui/js/invenio_previewer/bottom.js
@@ -6,6 +6,8 @@
  * under the terms of the MIT License; see LICENSE file for more details.
  */
 
+import $ from "jquery";
+
 $(".ui.button.embed-btn").click(function () {
   $("#embedModal").modal("show");
 });
