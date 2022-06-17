00a631b197268dfdc0896c70199026779cf1c518
Manuel Alejandro <mumox7@gmail.com>
2022-06-17 11:17:53 +0200

    jupyter: fixed previewer for jpynb

--- invenio_previewer/extensions/ipynb.py
+++ invenio_previewer/extensions/ipynb.py
@@ -28,7 +28,7 @@ def render(file):
     notebook = nbformat.reads(content.decode("utf-8"), as_version=4)
 
     html_exporter = HTMLExporter()
-    html_exporter.template_file = "basic"
+    html_exporter.template_file = "base"
     (body, resources) = html_exporter.from_notebook_node(notebook)
     return body, resources
 
@@ -41,7 +41,7 @@ def can_preview(file):
 def preview(file):
     """Render the IPython Notebook."""
     body, resources = render(file)
-    default_jupyter_nb_style = resources["inlining"]["css"][1]
+    default_jupyter_nb_style = resources["inlining"]["css"][0]
     return render_template(
         "invenio_previewer/ipynb.html",
         file=file,
