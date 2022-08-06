632215f4a51946197c7fb0b3db66dfa467f16008
Felipe Carlos <efelipecarlos@gmail.com>
2022-08-06 09:20:24 -0300

    video: adding more video formats (supported by html video tag)

--- invenio_previewer/extensions/simple_video.py
+++ invenio_previewer/extensions/simple_video.py
@@ -10,24 +10,32 @@
 
 from __future__ import absolute_import, print_function
 
-from flask import current_app, render_template
+from pathlib import Path
+from flask import render_template
 
 from ..proxies import current_previewer
 
-previewable_extensions = ["mp4"]
+previewable_extensions = ["mp4", "webm", "ogg"]
 
 
 def can_preview(file):
     """Determine if the given file can be previewed."""
-    supported_extensions = (".mp4",)
+    supported_extensions = (".mp4", ".webm", ".ogg")
     return file.has_extensions(*supported_extensions)
 
 
 def preview(file):
     """Render the appropriate template with embed flag."""
+
+    # extracting file extension
+    # ToDo: Maybe, `PreviewFile` (Invenio App RDM) can provides a 
+    #       `extension` or `type` attribute with the file extension.
+    extension = Path(file.filename).suffix.replace('.', '')
+
     return render_template(
         "invenio_previewer/simple_video.html",
         file=file,
+        extension=extension,
         js_bundles=current_previewer.js_bundles,
         css_bundles=current_previewer.css_bundles,
     )
