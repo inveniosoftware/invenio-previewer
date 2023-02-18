8a39b96d806cc91759081e0881b044350ebb82be
Christoph Ladurner <christoph.ladurner@tugraz.at>
2023-02-18 23:13:35 +0100

    move check_manifest configuration to setup.cfg.
    concentrate the configuration of all calls in one place

--- run-tests.sh
+++ run-tests.sh
@@ -4,12 +4,13 @@
 # This file is part of Invenio.
 # Copyright (C) 2016-2020 CERN.
 # Copyright (C) 2020 Northwestern University.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".*-requirements.txt" && \
-python -m setup extract_messages --output-file /dev/null
+python -m check_manifest && \
+python -m setup extract_messages --dry-run
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
 python -m pytest
 python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
