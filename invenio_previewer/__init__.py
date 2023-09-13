110dcdd007fa229feb43fcb0d7487b243e43cb0e
Nicola Tarocco <ntarocco@gmail.com>
2023-09-13 17:53:31 +0200

    global: fix wrong package version

--- invenio_previewer/__init__.py
+++ invenio_previewer/__init__.py
@@ -318,6 +318,6 @@ Now define the priority for all previewers by adding the newly created
 from .ext import InvenioPreviewer
 from .proxies import current_previewer
 
-__version__ = "1.3.9"
+__version__ = "1.5.0"
 
 __all__ = ("__version__", "current_previewer", "InvenioPreviewer")
