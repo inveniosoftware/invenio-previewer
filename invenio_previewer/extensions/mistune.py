3c91ad1a3d76d63766135ebc71b37bd6566254c5
Fatimah <fatimah.zulfiqar@cern.ch>
2024-01-25 14:51:56 +0100

    markdown: default message if not previewable

--- invenio_previewer/extensions/mistune.py
+++ invenio_previewer/extensions/mistune.py
@@ -10,7 +10,7 @@
 
 import bleach
 import mistune
-from flask import render_template
+from flask import current_app, render_template
 
 from ..proxies import current_previewer
 from ..utils import detect_encoding
@@ -22,8 +22,12 @@ def render(file):
     """Render HTML from Markdown file content."""
     with file.open() as fp:
         encoding = detect_encoding(fp, default="utf-8")
-        result = mistune.markdown(fp.read().decode(encoding))
-        return result
+        try:
+            result = mistune.markdown(fp.read().decode(encoding))
+            return result
+        except Exception as e:
+            current_app.logger.exception(str(e))
+            return None
 
 
 def can_preview(file):
@@ -33,10 +37,14 @@ def can_preview(file):
 
 def preview(file):
     """Render Markdown."""
-    return render_template(
-        "invenio_previewer/mistune.html",
-        file=file,
-        content=render(file),
-        js_bundles=current_previewer.js_bundles,
-        css_bundles=current_previewer.css_bundles,
-    )
+    content = render(file)
+    if content is not None:
+        return render_template(
+            "invenio_previewer/mistune.html",
+            file=file,
+            content=content,
+            js_bundles=current_previewer.js_bundles,
+            css_bundles=current_previewer.css_bundles,
+        )
+    else:
+        return render_template("invenio_previewer/default.html")
