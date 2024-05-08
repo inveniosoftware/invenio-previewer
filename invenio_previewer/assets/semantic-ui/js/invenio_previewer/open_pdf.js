671bdc6c168958a82ce87c807476ecf1ccd34467
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2024-05-08 16:22:10 +0200

    pdfjs: upgrade to pdf.js v4

--- invenio_previewer/assets/semantic-ui/js/invenio_previewer/open_pdf.js
+++ invenio_previewer/assets/semantic-ui/js/invenio_previewer/open_pdf.js
@@ -1,10 +1,94 @@
-// This file is part of InvenioRDM
-// Copyright (C) 2022 TU Wien.
-//
-// Invenio Theme TUW is free software; you can redistribute it and/or modify it
-// under the terms of the MIT License; see LICENSE file for more details.
+/* Copyright 2014 Mozilla Foundation
+ * Copyright 2024 TU Wien
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-document.addEventListener("DOMContentLoaded", function () {
-  var fileUri = document.getElementById("pdf-file-uri").value;
-  window.PDFView.open(fileUri);
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const { pdfjsLib, pdfjsViewer } = window;
+
+  // The workerSrc property shall be specified.
+  //
+  pdfjsLib.GlobalWorkerOptions.workerSrc = "/static/js/pdfjs/build/pdf.worker.mjs";
+
+  // Some PDFs need external cmaps.
+  //
+  const CMAP_URL = "/static/js/pdfjs/cmaps/";
+  const CMAP_PACKED = true;
+
+  // Get the PDF file's URL
+  const DEFAULT_URL = document.getElementById("pdf-file-uri").value;
+  const ENABLE_XFA = true;
+  const SEARCH_FOR = ""; // try "Mozilla";
+
+  const SANDBOX_BUNDLE_SRC = "/static/js/pdfjs/build/pdf.sandbox.mjs";
+
+  const container = document.getElementById("viewerContainer");
+
+  const eventBus = new pdfjsViewer.EventBus();
+
+  // (Optionally) enable hyperlinks within PDF files.
+  const pdfLinkService = new pdfjsViewer.PDFLinkService({
+    eventBus,
+  });
+
+  // (Optionally) enable find controller.
+  const pdfFindController = new pdfjsViewer.PDFFindController({
+    eventBus,
+    linkService: pdfLinkService,
+  });
+
+  // (Optionally) enable scripting support.
+  const pdfScriptingManager = new pdfjsViewer.PDFScriptingManager({
+    eventBus,
+    sandboxBundleSrc: SANDBOX_BUNDLE_SRC,
+  });
+
+  const pdfViewer = new pdfjsViewer.PDFViewer({
+    container,
+    eventBus,
+    linkService: pdfLinkService,
+    findController: pdfFindController,
+    scriptingManager: pdfScriptingManager,
+  });
+  pdfLinkService.setViewer(pdfViewer);
+  pdfScriptingManager.setViewer(pdfViewer);
+
+  eventBus.on("pagesinit", function () {
+    // We can use pdfViewer now, e.g. let's change default scale.
+    pdfViewer.currentScaleValue = "page-width";
+
+    // We can try searching for things.
+    if (SEARCH_FOR) {
+      eventBus.dispatch("find", { type: "", query: SEARCH_FOR });
+    }
+  });
+
+  // Loading document.
+  const loadingTask = pdfjsLib.getDocument({
+    url: DEFAULT_URL,
+    cMapUrl: CMAP_URL,
+    cMapPacked: CMAP_PACKED,
+    enableXfa: ENABLE_XFA,
+  });
+  (async function () {
+    const pdfDocument = await loadingTask.promise;
+    // Document loaded, specifying document for the viewer and
+    // the (optional) linkService.
+    pdfViewer.setDocument(pdfDocument);
+
+    pdfLinkService.setDocument(pdfDocument, null);
+  })();
 });
