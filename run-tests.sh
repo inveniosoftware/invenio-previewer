2ff928d040e1843d70f81515ef289a288c984c27
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-12-10 22:27:19 +0100

    tests: add postgresql, db is used

--- run-tests.sh
+++ run-tests.sh
@@ -4,12 +4,24 @@
 # This file is part of Invenio.
 # Copyright (C) 2016-2020 CERN.
 # Copyright (C) 2020 Northwestern University.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+
+# Quit on errors
+set -o errexit
+# Quit on unbound symbols
+set -o nounset
+# Always bring down docker services
+function cleanup() {
+    eval "$(docker-services-cli down --env)"
+}
+trap cleanup EXIT
+
 python -m check_manifest
 python -m setup extract_messages --output-file /dev/null
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
+eval "$(docker-services-cli up --db ${DB:-postgresql} --env)"
 python -m pytest
