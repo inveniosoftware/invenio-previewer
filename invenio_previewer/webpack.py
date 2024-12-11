36d7ff7e5df58f727c38c86b968d84a546cfab9e
Nicola <ntarocco@gmail.com>
2024-12-11 14:45:38 +0100

    Revert "pdfjs: upgrade to pdf.js v4"
    This reverts commit da99cd93441da285931cc04cf772adb02f3aadc4.

--- invenio_previewer/webpack.py
+++ invenio_previewer/webpack.py
@@ -37,8 +37,12 @@ previewer = WebpackThemeBundle(
             entry={
                 "papaparse_csv": "./js/invenio_previewer/csv_previewer/init.js",
                 "previewer_theme": "./js/invenio_previewer/previewer_theme.js",
+                "fullscreen_js": "./js/invenio_previewer/fullscreen.js",
                 "prism_js": "./js/invenio_previewer/prismjs.js",
                 "prism_css": "./scss/invenio_previewer/prismjs.scss",
+                "pdfjs_js": "./js/invenio_previewer/pdfjs.js",
+                "pdfjs_css": "./scss/invenio_previewer/pdfjs.scss",
+                "open_pdf": "./js/invenio_previewer/open_pdf.js",
                 "simple_image_css": "./scss/invenio_previewer/simple_image.scss",
             },
             dependencies={
@@ -47,35 +51,24 @@ previewer = WebpackThemeBundle(
                 "flightjs": "~1.5.1",
                 "font-awesome": "~4.5.0",
                 "jquery": "^3.3.1",
-                "pdfjs-dist": "^4.0",
+                "pdfjs-dist": "^1.4.192",
                 "prismjs": "^1.15.0",
             },
             aliases={
                 "@scss/invenio_previewer": "scss/invenio_previewer",
             },
-            copy=[
-                # Copy the pdfjs-dist artifacts from `node_modules` into `static`
-                {
-                    "from": "../node_modules/pdfjs-dist/build",
-                    "to": "../../static/js/pdfjs/build",
-                },
-                {
-                    "from": "../node_modules/pdfjs-dist/cmaps",
-                    "to": "../../static/js/pdfjs/cmaps",
-                },
-                {
-                    "from": "../node_modules/pdfjs-dist/web",
-                    "to": "../../static/js/pdfjs/web",
-                },
-            ],
         ),
         "semantic-ui": dict(
             entry={
                 "papaparse_csv": "./js/invenio_previewer/csv_previewer/init.js",
                 "previewer_theme": "./js/invenio_previewer/previewer_theme.js",
+                "fullscreen_js": "./js/invenio_previewer/fullscreen.js",
                 "prism_js": "./js/invenio_previewer/prismjs.js",
                 "prism_css": "./scss/invenio_previewer/prismjs.scss",
+                "pdfjs_js": "./js/invenio_previewer/pdfjs.js",
+                "open_pdf": "./js/invenio_previewer/open_pdf.js",
                 "bottom_js": "./js/invenio_previewer/bottom.js",
+                "pdfjs_css": "./scss/invenio_previewer/pdfjs.scss",
                 "zip_css": "./scss/invenio_previewer/zip.scss",
                 "bottom_css": "./scss/invenio_previewer/bottom.scss",
                 "simple_image_css": "./scss/invenio_previewer/simple_image.scss",
@@ -89,25 +82,10 @@ previewer = WebpackThemeBundle(
                 "font-awesome": "~4.5.0",
                 "jquery": "^3.3.1",
                 "papaparse": "^5.4.1",
+                "pdfjs-dist": "^1.4.192",
                 "prismjs": "^1.15.0",
                 "video.js": "^8.6.1",
-                "pdfjs-dist": "^4.0",
             },
-            copy=[
-                # Copy the pdfjs-dist artifacts from `node_modules` into `static`
-                {
-                    "from": "../node_modules/pdfjs-dist/build",
-                    "to": "../../static/js/pdfjs/build",
-                },
-                {
-                    "from": "../node_modules/pdfjs-dist/cmaps",
-                    "to": "../../static/js/pdfjs/cmaps",
-                },
-                {
-                    "from": "../node_modules/pdfjs-dist/web",
-                    "to": "../../static/js/pdfjs/web",
-                },
-            ],
         ),
     },
 )
