527ab3df5fe322bebf28c19bda8161495832a883
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2025-10-21 14:33:55 +0200

    📦 release: v3.4.1

--- invenio_previewer/__init__.py
+++ invenio_previewer/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of Invenio.
 # Copyright (C) 2016-2025 CERN.
 # Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -351,6 +352,6 @@ Now define the priority for all previewers by adding the newly created
 from .ext import InvenioPreviewer
 from .proxies import current_previewer
 
-__version__ = "3.4.0"
+__version__ = "3.4.1"
 
 __all__ = ("__version__", "current_previewer", "InvenioPreviewer")
